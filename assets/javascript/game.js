fb28e8e9be75744057739d7164635c11dc8f0c62
JHaarla <haarladesign@gmail.com>
2019-08-06 18:58:59 -0400

    adding game logic via jQuery. Code cleanup.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,24 +12,29 @@ var crystal1Value;
 var crystal2Value;
 var crystal3Value;
 
-var goalNumber;
-var yourTotal;
+var goalNumber = setGoal();
+var yourTotal = 0;
+
+var totalCounter = 0;
 
 var wins = 0;
 var losses = 0;
 
+var crystalValues = [setCrystal(), setCrystal(), setCrystal(), setCrystal()]
+console.log("crystal values array: " + crystalValues);
+
 var winsText = document.getElementById("winsAmount");
 var lossesText = document.getElementById("lossesAmount");
-var goalNumberText = document.getElementById("goalNumber");
-var totalNumberText = document.getElementById("yourTotal");
+var goalNumberText = $("#goalNumber");
+// var totalNumberText = document.getElementById("yourTotal");
 
-reset(); //runs the reset function to initialize the game or to reset it after a win or loss
+// reset(); //runs the reset function to initialize the game or to reset it after a win or loss
 
 
 // diplays wins, losses, goal number and user total 
 winsText.textContent = wins;
 lossesText.textContent = losses;
-goalNumberText.textContent = goalNumber;
+goalNumberText.text(goalNumber);
 totalNumberText.textContent = yourTotal;
 
 
@@ -39,6 +44,27 @@ console.log("Crystal 1 value is " + crystal1Value)
 console.log("Crystal 2 value is " + crystal2Value)
 console.log("Crystal 3 value is " + crystal3Value)
 
+// Create crystals for each index in crystalValues array and create a <img> tag with class="crystal-image" and crystal-value-data set to crystalValues[i]. Then, push to the DOM (our original empty crystal-container in the HTML) via .append. whew! :) 
+for (var i = 0; i < crystalValues.length; i++) {
+    var crystalImage = $("<img>");
+
+    crystalImage.addClass("crystal-image");
+    crystalImage.attr("src", ("assets/images/crystal-" + [i] + ".png"));
+    crystalImage.attr("crystal-value-data", crystalValues[i]);
+    $("#crystal-container").append(crystalImage);
+}
+
+// click event to trigger the next action(s). Also, this event listens to ALL crystals on the page (because they all have the same class...)
+$("crystal-image").on("click", function(){
+    //
+    var crystalValue = ($(this).attr("crystal-value-data"));
+    crystalValue = parseInt(crystalValue);
+
+    //
+    yourTotal += crystalValue;
+
+    $("#your")
+})
 
 
 // function to set goalNumber to a random value between 19 and 120
@@ -46,34 +72,44 @@ function setGoal() {
     return Math.floor(Math.random() * (120 - 19) + 19); 
 };
 
-// function to set crystal values between 1 and 12
-function setCrystal0() {
+// sets crystal value (for each index in the crystalValues array)
+function setCrystal() {
     return Math.floor(Math.random() * (12 - 1) + 1); 
 };
+
+
+
+
+
+// function to set crystal values between 1 and 12
+// function setCrystal0() {
+    // return Math.floor(Math.random() * (12 - 1) + 1); 
+// };
 // console.log("Crystal 0 value is " + crystal0Value)
 
-function setCrystal1() {
-    return Math.floor(Math.random() * (12 - 1) + 1); 
-};
+// function setCrystal1() {
+    // return Math.floor(Math.random() * (12 - 1) + 1); 
+// };
 // console.log("Crystal 1 value is " + crystal1Value)
 
-function setCrystal2() {
-    return Math.floor(Math.random() * (12 - 1) + 1); 
-};
+// function setCrystal2() {
+//     return Math.floor(Math.random() * (12 - 1) + 1); 
+// };
 // console.log("Crystal 2 value is " + crystal2Value)
 
-function setCrystal3() {
-    return Math.floor(Math.random() * (12 - 1) + 1); 
-};
+// function setCrystal3() {
+//     return Math.floor(Math.random() * (12 - 1) + 1); 
+// };
 // console.log("Crystal 3 value is " + crystal3Value)
 
 
 // reset function - runs at the beginning and every time the player wins or loses
 function reset() {
-    crystal0Value = setCrystal0();
-    crystal1Value = setCrystal1();
-    crystal2Value = setCrystal2();
-    crystal3Value = setCrystal3();
+    // crystal0Value = setCrystal0();
+    // crystal1Value = setCrystal1();
+    // crystal2Value = setCrystal2();
+    // crystal3Value = setCrystal3();
+    setCrystal();
     yourTotal = 0;
     goalNumber = setGoal();
 
@@ -84,10 +120,10 @@ function reset() {
 
 
 // reset(); //runs the reset function to initialize the game or to reset it after a win or loss
-console.log("goalNumber= " + goalNumber);
-console.log("Crystal 0 value is " + crystal0Value)
-console.log("Crystal 1 value is " + crystal1Value)
-console.log("Crystal 2 value is " + crystal2Value)
-console.log("Crystal 3 value is " + crystal3Value)
+// console.log("goalNumber= " + goalNumber);
+// console.log("Crystal 0 value is " + crystal0Value)
+// console.log("Crystal 1 value is " + crystal1Value)
+// console.log("Crystal 2 value is " + crystal2Value)
+// console.log("Crystal 3 value is " + crystal3Value)
 
 }); // *fin* do not delete
